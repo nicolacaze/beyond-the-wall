ed8933b2fe4c559f32338fce12c3ce96ad1c8ef4
Nicolas Lacaze <nicolas.lacaze@gmail.com>
2019-01-18 13:53:32 +0100

    Add game and game-over screens

--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
-var playBtn = document.querySelector('.play-btn');
+'use strict';
+
+var playBtn = document.querySelector('.splash-screen button');
 var splashScreen = document.querySelector('.splash-screen');
 var gameScreen = document.querySelector('.game-screen');
+var playAgainBtn = document.querySelector('.game-over-screen button');
+var gameOverScreen = document.querySelector('.game-over-screen');
+
 
 function removeSplashScreen() {
   splashScreen.classList.add('hide-splash-screen');
+}
+
+function buildGameScreen() {
+  gameScreen.classList.add('display-game-screen');
+
+  // Simulate end game after 5 seconds
+  setTimeout(function() {
+    removeGameScreen();
+    buildGameOverScreen();
+  }, 5000);
+  
+}
+
+function removeGameScreen() {
+  gameScreen.classList.remove('display-game-screen');
+}
+
+function buildGameOverScreen() {
+  gameOverScreen.classList.add('display-game-over');
+  
+}
+
+function removeGameOverScreen() {
+  gameOverScreen.classList.add('game-over');
 }
 
 playBtn.addEventListener('click', function() {
@@ -12,4 +41,13 @@ playBtn.addEventListener('click', function() {
   buildGameScreen();
 
   // start game
+
+  //End Game
+  buildGameOverScreen();
 });
+
+playAgainBtn.addEventListener('click', function(){
+
+});
+
+
