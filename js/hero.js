3cbcf8042ef82705311de25abfd5cc0f83403f8b
Nicolas Lacaze <nicolas.lacaze@gmail.com>
2019-01-19 14:11:32 +0100

    Implement bounceOnEdges() logic for Hero

--- js/hero.js
+++ js/hero.js
@@ -1,13 +1,15 @@
 'use strict';
 
 function Hero(canvas) {
+  this.canvas = canvas;
   this.context = canvas.getContext('2d');
   this.x = 100;
   this.y = 100;
-  this.directionX = 0;
+  this.directionX;
   this.directionY;
-  this.speed = 5;
-  this.image = new Image(64,64);
+  this.size = 64;
+  this.speed = 10;
+  this.image = new Image(this.size,this.size);
 }
 
 Hero.prototype.setDirection = function(direction) {
@@ -30,10 +32,78 @@ Hero.prototype.setDirection = function(direction) {
 }
 
 Hero.prototype.move = function(axis) {
-  if(axis === 'x') {
+
+  if(this.hasReachedLeft()) {
+    this.bounceOnEdges('left');
+  } else if(this.hasReachedRight()) {
+    this.bounceOnEdges('right');
+  } else if(this.hasReachedTop()) {
+    this.bounceOnEdges('top');
+  } else if(this.hasReachedBottom()) {
+    this.bounceOnEdges('bottom');
+  } else {
+
+    if(axis === 'x') {
+      this.x += this.directionX * this.speed;
+    } else if(axis === 'y') {
+      this.y += this.directionY * this.speed;
+    } 
+  } 
+}
+
+Hero.prototype.bounceOnEdges = function(side) {
+
+  switch(side) {
+    case 'left':
+    this.setDirection('right');
     this.x += this.directionX * this.speed;
-  } else if(axis === 'y') {
+    break;
+    case 'right':
+    this.setDirection('left');
+    this.x += this.directionX * this.speed;
+    break;
+    case 'top':
+    this.setDirection('down');
     this.y += this.directionY * this.speed;
+    break;
+    case 'bottom':
+    this.setDirection('up');
+    this.y += this.directionY * this.speed;
+    break;
+    default:
+    console.log('No bounce called');
+  }
+}
+
+Hero.prototype.hasReachedLeft = function() {
+  if(this.x <= 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+Hero.prototype.hasReachedRight = function() {
+  if (this.x >= this.canvas.width - this.size) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+Hero.prototype.hasReachedTop = function() {
+  if (this.y <= 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+Hero.prototype.hasReachedBottom = function() {
+  if (this.y >= this.canvas.height - this.size) {
+    return true;
+  } else {
+    return false;
   }
 }
 
